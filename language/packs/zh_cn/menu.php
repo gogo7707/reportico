c1f3b48f6cb2fe20b55e0714dad0287d741d9652
gogo7707 <jk_jyt@126.com>
2017-09-24 14:05:11 +0800

    The original translation sucks, the chinese language has update

--- language/packs/zh_cn/menu.php
+++ language/packs/zh_cn/menu.php
@@ -3,7 +3,7 @@ $locale_arr = array (
 "language" => "Chinese (simplified)",
 "template" => array (
         "T_CHOOSE_LANGUAGE" => "选择语言",
-        "T_GO" => "去",
+        "T_GO" => "前往",
         "T_LOGGED_IN_AS" => "登录为",
         "T_LOGIN" => "登录",
         "T_LOGOFF" => "注销",
@@ -11,7 +11,7 @@ $locale_arr = array (
         "T_CONFIG_PROJECT" => "配置项目",
         "T_CREATE_REPORT" => "创建报告",
         "T_ENTER_PROJECT_PASSWORD" => "输入项目密码",
-        "T_ENTER_PROJECT_PASSWORD_DEMO" => "输入项目密码。这些教程的密码是 <b>reportico</b>",
+        "T_ENTER_PROJECT_PASSWORD_DEMO" => "输入项目密码。这些项目的密码是 <b>reportico</b>",
         "T_UNABLE_TO_CONTINUE" => "",
 		"T_PASSWORD_ERROR" => "不正确的密码。再试一次。",
 ),
